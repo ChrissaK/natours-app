82c19bf4e831ff9338c600e7df7cbb1943fb4d01
ChrissaK <chrissa.kaza@gmail.com>
2023-02-14 18:21:38 +0000

    Data validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'],
       unique: true,
       trim: true,
+      minlength: [10, 'A tour name must have a minimum of 10 characters.'],
+      maxlength: [40, 'A tour name must have a maximum of 40 characters.'],
+      validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: {
       type: String,
@@ -23,12 +27,29 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty value can be either: easy, medium or difficult.',
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above or equal to 1.0'],
+      max: [5, 'Rating must be below or equal to 5.0'],
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price.'] },
     priceDiscount: {
       type: Number,
+      validate: {
+        validator: function (val) {
+          // if it returns false, there is an input error
+          // the this keyword only points to the current document on creation of a NEW document (not on update)
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below the regular price.',
+      },
     },
     summary: {
       type: String,
@@ -66,7 +87,7 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// DOCUMENT MIDDLEWARE, it runs before .save() and .create()
+// DOCUMENT MIDDLEWARE, it runs before .save() and .create() (NOT before .update())
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true }).replace("'", '');
   console.log(this);
