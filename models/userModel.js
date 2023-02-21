6ec429e68e2a7c4b9cb1c63930c27d4f523d388c
ChrissaK <chrissa.kaza@gmail.com>
2023-02-21 15:06:55 +0000

    User model and signup

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   //name, email, photo, password, passwordConfirm
@@ -27,9 +28,28 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password.'],
+    validate: {
+      // this only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
 });
 
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // passwordConfirm is only needed for validating the password, it doesn't need to be saved in the database
+  this.passwordConfirm = undefined;
+
+  next();
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
